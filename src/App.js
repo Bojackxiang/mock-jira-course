c47d9053948a847fa380f53ba41cdd0b5021d854
Alex <alex@q-83.com>
2022-07-25 19:51:39 +1000

    解决baseurl问题与测试

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import test from "utils/test.js";
 
 function App() {
+  test();
   return (
     <div className="App">
       <header className="App-header">
