f7db754d28310b9cbbec53464b943ca6d271f032
Alex <alex@q-83.com>
2022-07-28 15:31:30 +1000

    json mock 中间件的实施

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,17 @@
+module.exports = (req, res, next) => {
+  if (req.method === "POST" && req.path === "/login") {
+    const { username, password } = req.body;
+    if (username === "admin" && password === "admin") {
+      res.status(200).json({
+        user: {
+          token: "123",
+        },
+      });
+    } else {
+      res.status(400).json({
+        message: "wrong password",
+      });
+    }
+  }
+  next();
+};
