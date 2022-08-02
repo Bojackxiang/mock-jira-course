6fb0c4a72d4d2d864111d2fba343c949cb6f40ce
Alex <alex@q-83.com>
2022-08-02 20:24:29 +1000

    如果没有任何路由匹配到的话，使用route + navigate 来进行跳转

--- src/screens/ProjectScreen.jsx
+++ src/screens/ProjectScreen.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
+
+import EpicScreen from "./EpicScreen";
+import KanbanScreen from "./KanbanScreen";
+import TestScreen from "./TestScreen";
 
 const ProjectScreen = () => {
-  return <div>ProjectScreen</div>;
+  return (
+    <div>
+      <div>ProjectScreen</div>
+      <Link to={"kanban"}>看板</Link>
+      <Link to={"epic"}>任务组</Link>
+
+      {console.log(window.location.pathname + "/kanban")}
+
+      <Routes>
+        {/* /projects/:projectId/kanban */}
+        <Route path="/kanban" element={<KanbanScreen />} />
+        {/* /projects/:projectId/epic */}
+        <Route path="/epic" element={<EpicScreen />} />
+        {/* 进来这个页面之后直接就先试看板 */}
+        <Route
+          path="*"
+          element={<Navigate to={window.location.pathname + "/kanban"} />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default ProjectScreen;
